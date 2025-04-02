b8637253871edda60b317acdd19aa4a37e1aa83c
Irwingduran <86335744+Irwingduran@users.noreply.github.com>
2025-04-01 22:41:38 -0600

    Update Home.jsx

--- src/pages/service5/Home.jsx
+++ src/pages/service5/Home.jsx
@@ -68,7 +68,7 @@ const Home = () => {
 
       {/* Llamada a la Acción */}
       <div className="mt-8 flex justify-center items-center">
-        <a href="https://api.whatsapp.com/send/?phone=5212226398763&text=Hola+Dr.+me+gustar%C3%ADa+agendar+una+consulta+sobre+biopsia+prost%C3%A1tica&type=phone_number&app_absent=0">
+        <a href="https://www.doctoralia.com.mx/ivo-humberto-pineda-somodevilla/urologo/puebla">
         <button className="bg-[#0284C7] text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-300">
           ¡Agenda tu Consulta Ahora!
         </button>
